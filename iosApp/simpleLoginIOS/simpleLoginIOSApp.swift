704b29fe23cc5052cc609c493190e52ed5392639
Jacee <jacee_dai@163.com>
2023-03-10 18:10:32 +0800

    ios: create,config and build

---
+++ iosApp/simpleLoginIOS/simpleLoginIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleLoginIOSApp.swift
+//  simpleLoginIOS
+//
+//  Created by Jacee Dai on 2023/3/10.
+//
+
+import SwiftUI
+
+@main
+struct simpleLoginIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
