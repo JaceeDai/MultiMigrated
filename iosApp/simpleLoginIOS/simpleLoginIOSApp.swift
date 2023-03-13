f1478b2d495c82d67479cc5aa90f3042ad112530
Jacee <jacee_dai@163.com>
2023-03-13 15:04:54 +0800

    ios: import shared and update ui

--- iosApp/simpleLoginIOS/simpleLoginIOSApp.swift
+++ iosApp/simpleLoginIOS/simpleLoginIOSApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import shared
 
 @main
 struct simpleLoginIOSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+        
+            ContentView(viewModel: .init(loginRepository: LoginRepository(dataSource: LoginDataSource()), loginValidator: LoginDataValidator()))
         }
     }
 }
